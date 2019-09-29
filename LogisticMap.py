95251823e5981759e41bb4ce94f5e849cc736016
Art Mountain <artmountain@btinternet.com>
2019-09-29 16:00:30 +0100

    First cut of k-slider in logistic map runner

--- LogisticMap.py
+++ LogisticMap.py
@@ -6,23 +6,33 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.widgets import Slider
 
 # Definition of parameters
 nPoints = 100
+kMin = 0
+kMax = 10
 k = 3.5
 
 # Starting value
-x = 0.25
+xInit = 0.25
 
 # lists to store values of t and x
 t, xdata = [], []
-for j in range(nPoints):
-    # Store current values
-    t.append(j)
-    xdata.append(x)
 
-    # Calculate new values
-    x = k * x * (1 - x)
+
+def setUpData():
+    global t, xdata, k
+    t = []
+    xdata = []
+    x = xInit
+    for j in range(nPoints):
+        # Store current values
+        t.append(j)
+        xdata.append(x)
+
+        # Calculate new values
+        x = k * x * (1 - x)
 
 
 # initialization function for time series
@@ -38,15 +48,31 @@ def animateLogisticMap(i):
     return line,
 
 
+# Calculate the data for the plot
+setUpData()
+
 # Plot setup - set the axes to be 1.25x the largest value so we fit the plot
 plt.style.use('dark_background')
-fig = plt.figure()
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.15)
 plt.axis('on')
 tMax = max(t)
 xMax = max(xdata)
+plt.title('Logistic map evolution')
+
+# Slider for k value
+axcolor = 'lightgoldenrodyellow'
+axSlider = plt.axes([0.15, 0.05, 0.65, 0.03], facecolor=axcolor)
+kSlider = Slider(axSlider, 'k Value', 0.1, 10.0, valinit=k, valstep=(kMax - kMin) / 10)
+#kSlider  = Slider(axSlider, 'k Value', 0.1, 10.0, valinit=k, valstep=0.1)
+
+def update(val):
+    global k
+    k = kSlider.val
+    fig.canvas.draw_idle()
+kSlider.on_changed(update)
 
 # Call the animator
-plt.title('Logistic map evolution')
 ax = plt.axes(xlim=(0, tMax), ylim=(0, xMax * 1.25))
 animatedPlot = animation.FuncAnimation(fig, animateLogisticMap, init_func=initLogisticMap,
                                        frames=nPoints, interval=10, blit=True, repeat=False)
