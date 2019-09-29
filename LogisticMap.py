e0d81758c1035f5ba5bf7e98ab2ca37cdf98f973
Art Mountain <artmountain@btinternet.com>
2019-09-29 19:37:37 +0100

    Working k-slider in logistic map runner

--- LogisticMap.py
+++ LogisticMap.py
@@ -10,8 +10,8 @@ from matplotlib.widgets import Slider
 
 # Definition of parameters
 nPoints = 100
-kMin = 0
-kMax = 10
+kMin = 1
+kMax = 4
 k = 3.5
 
 # Starting value
@@ -58,26 +58,35 @@ plt.subplots_adjust(bottom=0.15)
 plt.axis('on')
 tMax = max(t)
 xMax = max(xdata)
+
 plt.title('Logistic map evolution')
 
 # Slider for k value
 axcolor = 'lightgoldenrodyellow'
 axSlider = plt.axes([0.15, 0.05, 0.65, 0.03], facecolor=axcolor)
-kSlider = Slider(axSlider, 'k Value', 0.1, 10.0, valinit=k, valstep=(kMax - kMin) / 10)
-#kSlider  = Slider(axSlider, 'k Value', 0.1, 10.0, valinit=k, valstep=0.1)
+kSlider = Slider(axSlider, 'k Value', kMin, kMax, valinit=k, valstep=(kMax - kMin) / 1000)
+
 
 def update(val):
     global k
     k = kSlider.val
     fig.canvas.draw_idle()
+    x = xInit
+    for j in range(nPoints):
+        # Store current values then calculate new values
+        xdata[j] = x
+        x = k * x * (1 - x)
+    fig.canvas.draw_idle()
+
+
 kSlider.on_changed(update)
 
 # Call the animator
-ax = plt.axes(xlim=(0, tMax), ylim=(0, xMax * 1.25))
+axes = plt.axes(xlim=(0, tMax), ylim=(0, xMax * 1.25))
 animatedPlot = animation.FuncAnimation(fig, animateLogisticMap, init_func=initLogisticMap,
-                                       frames=nPoints, interval=10, blit=True, repeat=False)
+                                       frames=nPoints, interval=10, blit=True, repeat=True)
 
-line, = ax.plot([], [], lw=2)
+line, = axes.plot([], [], lw=2)
 plt.show()
 
 # Uncomment to save the animation as mp4 video file
